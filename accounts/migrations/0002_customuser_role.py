4ffe8de6db6f824774ee508a0602d3b3eee01bda
m-gaddafi <muhammedgaddafi01@gmail.com>
2026-09-18 17:09:37 +0300

    ll

--- accounts/migrations/0002_customuser_role.py
+++ accounts/migrations/0002_customuser_role.py
@@ -1,15 +1,35 @@
 from django.db import migrations, models
 
 
+def add_role_column(apps, schema_editor):
+    CustomUser = apps.get_model('accounts', 'CustomUser')
+    with schema_editor.connection.cursor() as cursor:
+        columns = {
+            column.name
+            for column in schema_editor.connection.introspection.get_table_description(
+                cursor,
+                CustomUser._meta.db_table,
+            )
+        }
+    if 'role' not in columns:
+        field = models.CharField(default='user', max_length=20)
+        field.set_attributes_from_name('role')
+        schema_editor.add_field(CustomUser, field)
+
 class Migration(migrations.Migration):
     dependencies = [
         ('accounts', '0001_initial'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='customuser',
-            name='role',
-            field=models.CharField(default='user', max_length=20),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[migrations.RunPython(add_role_column, migrations.RunPython.noop)],
+            state_operations=[
+                migrations.AddField(
+                    model_name='customuser',
+                    name='role',
+                    field=models.CharField(default='user', max_length=20),
+                ),
+            ],
         ),
     ]
--- accounts/migrations/0003_customuser_permissions.py
+++ accounts/migrations/0003_customuser_permissions.py
@@ -1,6 +1,22 @@
 from django.db import migrations, models
 
 
+def add_permission_columns(apps, schema_editor):
+    CustomUser = apps.get_model('accounts', 'CustomUser')
+    with schema_editor.connection.cursor() as cursor:
+        columns = {
+            column.name
+            for column in schema_editor.connection.introspection.get_table_description(
+                cursor,
+                CustomUser._meta.db_table,
+            )
+        }
+    for field_name in ('can_paste_payments', 'can_verify_payments'):
+        if field_name not in columns:
+            field = models.BooleanField(default=False)
+            field.set_attributes_from_name(field_name)
+            schema_editor.add_field(CustomUser, field)
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -8,14 +24,19 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='customuser',
-            name='can_paste_payments',
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AddField(
-            model_name='customuser',
-            name='can_verify_payments',
-            field=models.BooleanField(default=False),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[migrations.RunPython(add_permission_columns, migrations.RunPython.noop)],
+            state_operations=[
+                migrations.AddField(
+                    model_name='customuser',
+                    name='can_paste_payments',
+                    field=models.BooleanField(default=False),
+                ),
+                migrations.AddField(
+                    model_name='customuser',
+                    name='can_verify_payments',
+                    field=models.BooleanField(default=False),
+                ),
+            ],
         ),
     ]
